0645e7291b5c0de2d71891626077b353aab8a79b
MakisKovanidis <makis_kovanidis@hotmail.com>
2017-05-06 14:25:47 +0300

    Unstable Signin

--- app/src/main/java/com/example/makis/smartpark/MainActivity.java
+++ app/src/main/java/com/example/makis/smartpark/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.makis.smartpark;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
