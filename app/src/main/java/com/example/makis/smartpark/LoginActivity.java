75f1c5d1fc2e8591987bdca2691c72a630ca2e64
MakisKovanidis <makis_kovanidis@hotmail.com>
2017-05-06 09:35:20 +0300

    add secondActivity

--- app/src/main/java/com/example/makis/smartpark/LoginActivity.java
+++ app/src/main/java/com/example/makis/smartpark/LoginActivity.java
@@ -1,13 +1,37 @@
 package com.example.makis.smartpark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+   /*public Button but2;
+
+    public void init(){
+        but2=(Button)findViewById(R.id.but2);
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy= new Intent(LoginActivity.this,MainActivity.class);
+
+                startActivity(toy);
+            }
+        });
+    }*/
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //init();
     }
 }
