50da336bf0f266e4030dd06160ce30c4c66e28fe
MakisKovanidis <makis_kovanidis@hotmail.com>
2017-05-06 23:26:38 +0300

    Add Button FInd Park

--- app/src/main/java/com/example/makis/smartpark/MainLayerActivity.java
+++ app/src/main/java/com/example/makis/smartpark/MainLayerActivity.java
@@ -1,13 +1,27 @@
 package com.example.makis.smartpark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainLayerActivity extends AppCompatActivity {
-
+Button findpark;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_layer);
+        findpark=(Button)findViewById(R.id.fndpark);
+        findpark.setOnClickListener( new Button.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Intent myIntent=new Intent(v.getContext(),MapsActivity.class);
+                startActivity(myIntent);
+
+
+            }
+        });
     }
 }
